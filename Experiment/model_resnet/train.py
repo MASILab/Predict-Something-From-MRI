2832e6792641e61fda03c5881209754d5f6783e6
chenyugoal <chenyu_gao@outlook.com>
2023-07-20 22:48:49 -0500

    optimize RAM

--- Experiment/model_resnet/train.py
+++ Experiment/model_resnet/train.py
@@ -27,6 +27,7 @@ if __name__ == "__main__":
     # user inputs
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_name", type=str, default='resnet10_noMLP', help="Name of the model, see models.py for the full list of models")
+    parser.add_argument("--fold", nargs='+', type=int, default=[1,2,3,4,5], help="Fold(s) to train, e.g.'--fold 1 2 3 4 5', default to 1 2 3 4 5")
     parser.add_argument("--num_epochs", type=int, default=100, help="Number of epochs, default to 100")
     parser.add_argument("--batch_size", type=int, default=8, help="Batch size, default to 8")
     parser.add_argument("--model_save_dir", type=str, default='/home-local/Projects/Predict_Age_Models', help="Directory to save the model weights, default to /home-local/Projects/Predict_Age_Models")
@@ -41,7 +42,7 @@ if __name__ == "__main__":
     train_csv = datasplit_folder / 'train.csv'
     df = pd.read_csv(train_csv)
 
-    for fold_idx in range(1,6):
+    for fold_idx in args.fold:
 
         # subject-level splitting
         subjects_train = np.load((datasplit_folder/'train_subjects_t_fold_{}.npy'.format(fold_idx)), allow_pickle=True)
@@ -53,13 +54,13 @@ if __name__ == "__main__":
         dataset_train = AgePredictionDataset(df_train)
         dataset_val = AgePredictionDataset(df_val)
         
-        dataloader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True)
-        dataloader_val = DataLoader(dataset_val, batch_size=batch_size, shuffle=True)
+        dataloader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
+        dataloader_val = DataLoader(dataset_val, batch_size=batch_size*2, shuffle=False, num_workers=4, pin_memory=True)
 
         print("Start fold-{}\nTraining set:\n{}\nValidation set:\n{}\n".format(fold_idx, df_train, df_val))
         
         # TODO: think more on this block, there are many options
-        model = select_model(args.model_name).to(device)
+        model = select_model(args.model_name).to(device, non_blocking=True)
         optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
         scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[2, 6, 14, 30], gamma=0.2, last_epoch=-1, verbose=True)
         loss_fn = torch.nn.MSELoss()
@@ -69,17 +70,18 @@ if __name__ == "__main__":
         if not model_save_dir.is_dir():
             subprocess.run(['mkdir', '-p', model_save_dir])
         
-        # print(sum(torch.numel(param) for param in model.parameters()))
         for epoch in range(num_epochs):
             model.train()
             epoch_loss = 0.0
             for fa, md, sex, age in tqdm(dataloader_train):                
-                fa, md, sex, age = fa.to(device), md.to(device), sex.to(device), age.to(device)
-                
+                fa, md, sex, age = fa.to(device, non_blocking=True), md.to(device, non_blocking=True), sex.to(device, non_blocking=True), age.to(device, non_blocking=True)
                 input_img = torch.cat((fa,md), dim=1)
+                
                 optimizer.zero_grad()
-                output = model(input_img, sex)
-                loss = loss_fn(output, age.view(-1, 1))
+
+                with torch.autocast():
+                    output = model(input_img, sex)
+                    loss = loss_fn(output, age.view(-1, 1))
                 
                 loss.backward()
                 optimizer.step()
@@ -95,7 +97,7 @@ if __name__ == "__main__":
             val_loss = 0.0
             with torch.no_grad():
                 for fa_val, md_val, sex_val, age_val in dataloader_val:
-                    fa_val, md_val, sex_val, age_val = fa_val.to(device), md_val.to(device), sex_val.to(device), age_val.to(device)
+                    fa_val, md_val, sex_val, age_val = fa_val.to(device, non_blocking=True), md_val.to(device, non_blocking=True), sex_val.to(device, non_blocking=True), age_val.to(device, non_blocking=True)
 
                     input_img_val = torch.cat((fa_val,md_val), dim=1)
                     output_val = model(input_img_val, sex_val)
