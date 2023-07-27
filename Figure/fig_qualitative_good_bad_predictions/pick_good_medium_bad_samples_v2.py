3f654b8799b8ab961a487c2a138b0ec565be424a
chenyugoal <chenyu_gao@outlook.com>
2023-07-27 10:33:07 -0500

    v2, more picky

--- Figure/fig_qualitative_good_bad_predictions/pick_good_medium_bad_samples_v2.py
+++ Figure/fig_qualitative_good_bad_predictions/pick_good_medium_bad_samples_v2.py
@@ -1,3 +1,8 @@
+# Less randomness, more picky.
+# 
+# Author: Chenyu Gao
+# Date: Jul 27, 2023
+
 import pdb
 import pandas as pd
 import numpy as np
@@ -23,20 +28,22 @@ def get_axial_images(dir, slice_id=86):
 
 df = pd.read_csv('/nfs/masi/gaoc11/projects/Predict-Something-From-MRI/Experiment/model_resnet/data/model_evaluation/resnet18_MLP_64/fold-5/resnet18_MLP_64_fold-5_prediction_test_healthy.csv')
 df['Difference'] = df['Age_predicted'] - df['Age_gt']
-
+# pdb.set_trace()
 list_df_subset = [
-    df.loc[(df['Age']<60)&(df['Difference'].abs()<1),],
-    df.loc[(df['Age']<60)&(df['Age_predicted']>60)&(df['Age_predicted']<70),],
-    df.loc[(df['Age']<60)&(df['Age_predicted']>70),],
-    df.loc[(df['Age']>60)&(df['Age']<70)&(df['Age_predicted']<60)&(df['Difference']<-8),],
-    df.loc[(df['Age']>60)&(df['Age']<70)&(df['Difference'].abs()<1),],
-    df.loc[(df['Age']>60)&(df['Age']<70)&(df['Age_predicted']>70)&(df['Difference']>8),],
-    df.loc[(df['Age']>70)&(df['Age_predicted']<60),],
-    df.loc[(df['Age']>70)&(df['Age_predicted']>60)&(df['Age_predicted']<70),],
-    df.loc[(df['Age']>70)&(df['Difference'].abs()<0.2),],
+    df.loc[(df['Age']>47)&(df['Age']<53)&(df['Difference'].abs()<3),],
+    df.loc[(df['Age']>47)&(df['Age']<53)&(df['Difference']>5)&(df['Difference']<10),],
+    df.loc[(df['Age']>47)&(df['Age']<53)&(df['Difference']>10),],
+
+    df.loc[(df['Age']>62)&(df['Age']<68)&(df['Difference']<-10),],
+    df.loc[(df['Age']>62)&(df['Age']<68)&(df['Difference'].abs()<1),],
+    df.loc[(df['Age']>62)&(df['Age']<68)&(df['Age_predicted']>75)&(df['Difference']>10),],
+
+    df.loc[(df['Age']>77)&(df['Age']<83)&(df['Age_predicted']<65),],
+    df.loc[(df['Age']>77)&(df['Age']<83)&(df['Age_predicted']<73)&(df['Age_predicted']>71.5),],
+    df.loc[(df['Age']>77)&(df['Age']<83)&(df['Difference'].abs()<1),],
 ]
 
-for seed in tqdm(range(100)):
+for seed in tqdm(range(3)):
     
     fig, axes = plt.subplots(ncols=6,nrows=3,figsize=(6.5,4))
     
@@ -70,7 +77,7 @@ for seed in tqdm(range(100)):
         ax.set_yticks([])  # Remove y-axis ticks
         
     plt.subplots_adjust(wspace=0, hspace=0)
-    fig.savefig(f'/nfs/masi/gaoc11/projects/Predict-Something-From-MRI/Figure/fig_qualitative_good_bad_predictions/figs_v1/seed_{seed}.png',
+    fig.savefig(f'/nfs/masi/gaoc11/projects/Predict-Something-From-MRI/Figure/fig_qualitative_good_bad_predictions/figs_v2/seed_{seed}.png',
                 dpi=600,
                 bbox_inches='tight')
     plt.close('all')
